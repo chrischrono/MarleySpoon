f7a69a478dd0fe3bcbc7399881eebe1bbedc0654
Christian <chris_chrono@yahoo.com>
2019-05-31 05:34:53 +0700

    Add RecipeDetailViewController n its design layout

---
+++ MarleySpoon/View/RecipeDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeDetailViewController.swift
+//  MarleySpoon
+//
+//  Created by Christian on 30/05/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    @IBOutlet private weak var previewImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var tagsLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var chefLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
