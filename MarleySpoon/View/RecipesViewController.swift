9b94356e7a8ce9f58c26a3dba35be51baeacae96
Christian <chris_chrono@yahoo.com>
2019-05-30 18:57:31 +0700

    Add layout design for RecipesViewController

--- MarleySpoon/View/RecipesViewController.swift
+++ MarleySpoon/View/RecipesViewController.swift
@@ -11,6 +11,12 @@ import Contentful
 
 class RecipesViewController: UIViewController {
     
+    @IBOutlet private weak var recipeTableView: UITableView!
+    private let refreshControl = UIRefreshControl()
+    @IBOutlet private weak var statusView: UIView!
+    @IBOutlet private weak var statusViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var statusLabel: UILabel!
+    @IBOutlet private weak var loadingView: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +25,74 @@ class RecipesViewController: UIViewController {
         
     }
 
+}
+
+
+
+//MARK:- private func
+extension RecipesViewController {
+    private func initView() {
+        initTableView()
+        statusViewBottomConstraint.constant = statusView.bounds.height
+    }
+    
+    private func showStatusView(_ status: String?) {
+        guard let status = status else {
+            hideStatusView()
+            return
+        }
+        statusLabel.text = status//.localized()
+        statusViewBottomConstraint.constant = 0
+        UIView.animate(withDuration: 0.2, animations: {
+            self.view.layoutIfNeeded()
+        }) { [weak self] (_) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                self?.hideStatusView()
+            })
+        }
+    }
+    private func hideStatusView() {
+        statusViewBottomConstraint.constant = statusView.bounds.height
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func showLoadingView(_ isLoading: Bool) {
+        if isLoading {
+            loadingView.startAnimating()
+        } else {
+            loadingView.stopAnimating()
+            refreshControl.endRefreshing()
+        }
+    }
+}
+
+//MARK:- tableView func
+extension RecipesViewController: UITableViewDataSource {
+    private func initTableView() {
+        recipeTableView.register(UINib(nibName: "RecipeViewCell", bundle: nil), forCellReuseIdentifier: "RecipeViewCell")
+        
+        refreshControl.addTarget(self, action:
+            #selector(handleReshresh(_:)),
+                                 for: UIControl.Event.valueChanged)
+        
+        refreshControl.tintColor = UIColor.blue
+        recipeTableView.addSubview(refreshControl)
+    }
+    
+    @objc private func handleReshresh(_ refreshControl: UIRefreshControl?) {
+        //recipesViewModel.getCurrentLocation()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0//recipesViewModel.getFilteredCountryCount()
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeViewCell", for: indexPath) as! CountryViewCellRecipeViewCell        cell.configureCell(model: countrySearchViewModel.getRecipeCellViewModel(at: indexPath.row))
+        return cell*/
+        return UITableViewCell()
+    }
+    
 }
 
---
+++ MarleySpoon/View/ViewCell/RecipeViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeViewCell.swift
+//  MarleySpoon
+//
+//  Created by Christian on 30/05/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewCell: UITableViewCell {
+    @IBOutlet private weak var previewImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
